05269f8465db53e22818789fefa3f540c6478c4c
Daniela <danielatomoenk@gmail.com>
2023-01-04 12:42:15 -0300

    Add efeito acordeão em js

--- js/db-home.js
+++ js/db-home.js
@@ -1,12 +1,39 @@
-// Arrow
-const arrow = document.querySelector('db-arrow-back');
+//Accordions
+const firstBtn = document.querySelector('#first-btn');
+const secondBtn = document.querySelector('#second-btn');
+const thirdBtn = document.querySelector('#third-btn');
 
-// const abc = document.querySelector('.what-we-do .title');
-// const position = abc.getBoundingClientRect();
-// console.log(position)
+const descriptions = document.querySelectorAll('.db-accordion .description');
+const descriptionOne = document.querySelector('#first');
+const descriptionTwo = document.querySelector('#second');
+const descriptionThree = document.querySelector('#third');
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY = 150) {
-        arrow.classList.remove('on-top')
+firstBtn.addEventListener('click', () => {
+    descriptionOne.classList.toggle('-open');
+    
+    for (let i = 0; i < descriptions.length; i++) {
+        if (descriptions[i] !== descriptionOne && descriptions[i].classList.contains('-open')) {
+            descriptions[i].classList.remove('-open');
+        }
+    }
+});
+
+secondBtn.addEventListener('click', () => {
+    descriptionTwo.classList.toggle('-open');
+
+    for (let i = 0; i< descriptions.length; i++) {
+        if (descriptions[i] !== descriptionTwo && descriptions[i].classList.contains('-open')) {
+            descriptions[i].classList.remove('-open');
+        }
+    }
+})
+
+thirdBtn.addEventListener('click', () => {
+    descriptionThree.classList.toggle('-open');
+
+    for (let i = 0; i<descriptions.length; i++) {
+        if (descriptions[i] !== descriptionThree && descriptions[i].classList.contains('-open')) {
+            descriptions[i].classList.remove('-open');
+        }
     }
 })
