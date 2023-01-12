bcb9fb137f0919344f571415572ddb3653fe13d8
Daniela <danielatomoenk@gmail.com>
2023-01-12 01:40:30 -0300

    Add de eventos ao html e fim do projeto?

--- js/input-validation.js
+++ js/input-validation.js
@@ -2,16 +2,12 @@
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const birthDateInput = document.querySelector('#birthdate');
-const form1 = document.querySelector('#form1');
-const nextBtn = document.querySelector('#next-btn');
 
 const firstStep = document.querySelector('#first-step');
 const secondStep = document.querySelector('#second-step');
 const finalPage = document.querySelector('#final-page');
 
-form1.addEventListener('submit', (e) => {
-    e.preventDefault();
-
+function validationFirstStep() {
     if (!nameInput.value) {
         alert('Favor preencher seu nome.');
         return;
@@ -33,8 +29,8 @@ form1.addEventListener('submit', (e) => {
         return;
     }
 
-    nextBtn.addEventListener('click', nextPage());
-})
+    nextPage();
+}
 
 function emailValidation(email) {
     const emailRegex = new RegExp (
@@ -68,20 +64,15 @@ const cardNameInput = document.querySelector('#card-name');
 const dueDateInput = document.querySelector('#due-date');
 const cvvInput = document.querySelector('#cvv');
 
-const form2 = document.querySelector('#form2');
-const confirmBtn = document.querySelector('#confirm-btn');
-
-cardNumberInput.addEventListener('input', () => {
+function validCardNumber() {
     cardNumberInput.value = cardNumberInput.value.replace(/[^0-9]g, ""/);
-})
+}
 
-cvvInput.addEventListener('input', () => {
+function validCvvNumber() {
     cvvInput.value = cvvInput.value.replace(/[^0-9]g, ""/).slice(0, 3);
-})
+}
 
-form2.addEventListener('submit', (e) => {
-    e.preventDefault();
-
+function validationFinalStep() {
     if (!cardNumberInput.value) {
         alert('Favor preencher o número de seu cartão.');
         return;
@@ -111,8 +102,8 @@ form2.addEventListener('submit', (e) => {
         return;
     };
 
-    confirmBtn.addEventListener('click', finalStep())
-})
+    finalStep();
+}
 
 function checkCardNumber (cnumber) {
     const length = cnumber.length;
---
+++ js/menu-btn.js
@@ -0,0 +1,5 @@
+const menuMobile = document.querySelector('.nav-menu');
+
+function toggleMenu () {
+    menuMobile.classList.toggle('-active');
+}
