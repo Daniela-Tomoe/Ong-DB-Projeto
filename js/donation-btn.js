b2069dd62104c0cafcd477a3a82276a8b3c04477
Daniela <danielatomoenk@gmail.com>
2023-01-11 21:14:39 -0300

    add e alterações de js

--- js/donation-btn.js
+++ js/donation-btn.js
@@ -1,16 +1,25 @@
 const plusBtn = document.querySelector('#plus');
 const minusBtn = document.querySelector('#minus');
 const donationValue = document.querySelector('#value');
+const valueConfirmation = document.querySelector('#value-confirmation');
+const numberOfDogs = document.querySelector('#dog-number');
 
 plusBtn.addEventListener('click', () => {
     const currentNumber = parseInt(donationValue.textContent);
     const newNumber = currentNumber + 10;
     donationValue.textContent = newNumber;
+
+    valueConfirmation.textContent = donationValue.textContent;
+
+    numberOfDogs.textContent = newNumber / 10;
 })
 
 minusBtn.addEventListener('click', () => {
     const currentNumber = parseInt(donationValue.textContent);
     const newNumber = currentNumber - 10;
     if (newNumber >= 0 ) {
-        donationValue.textContent = newNumber; }
+        donationValue.textContent = newNumber; 
+        valueConfirmation.textContent = donationValue.textContent;
+        numberOfDogs.textContent = newNumber / 10;
+    }
 })
--- js/input-validation.js
+++ js/input-validation.js
@@ -2,14 +2,14 @@
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const birthDateInput = document.querySelector('#birthdate');
-const form = document.querySelector('#form1');
+const form1 = document.querySelector('#form1');
 const nextBtn = document.querySelector('#next-btn');
 
 const firstStep = document.querySelector('#first-step');
 const secondStep = document.querySelector('#second-step');
 const finalPage = document.querySelector('#final-page');
 
-form.addEventListener('submit', (e) => {
+form1.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (!nameInput.value) {
@@ -63,3 +63,77 @@ function nextPage() {
 }
 
 // Second step
+const cardNumberInput = document.querySelector('#card-number');
+const cardNameInput = document.querySelector('#card-name');
+const dueDateInput = document.querySelector('#due-date');
+const cvvInput = document.querySelector('#cvv');
+
+const form2 = document.querySelector('#form2');
+const confirmBtn = document.querySelector('#confirm-btn');
+
+cardNumberInput.addEventListener('input', () => {
+    cardNumberInput.value = cardNumberInput.value.replace(/[^0-9]g, ""/);
+})
+
+cvvInput.addEventListener('input', () => {
+    cvvInput.value = cvvInput.value.replace(/[^0-9]g, ""/).slice(0, 3);
+})
+
+form2.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!cardNumberInput.value) {
+        alert('Favor preencher o número de seu cartão.');
+        return;
+    } if (!checkCardNumber(cardNumberInput.value)) {
+        alert('Favor preencher o número de seu cartão corretamente');
+        return;
+    }
+
+    if (!cardNameInput.value) {
+        alert('Favor preencher o nome impresso no cartão.');
+        return;
+    }
+
+    if(!dueDateInput.value) {
+        alert('Favor preencher o vencimento do cartão.');
+        return;
+    } if (!dueDateValidation(dueDateInput.value)) {
+        alert('Favor preencher o vencimento do cartão corretamente.');
+        return;
+    }
+
+    if (!cvvInput.value) {
+        alert('Favor preencher o cvv do cartão.');
+        return;
+    } if (!checkCvv(cvvInput.value)) {
+        alert('Favor preencher o cvv do cartão corretamente.');
+        return;
+    };
+
+    confirmBtn.addEventListener('click', finalStep())
+})
+
+function checkCardNumber (value) {
+    value.length = 16;
+    return true;
+}
+
+function dueDateValidation (value) {
+    const dateRegex = new RegExp (
+        /^[0-9]{2}\/[0-9]{2}$/
+    )
+    if (dateRegex.test(value)) {
+        return true
+    }
+}
+
+function checkCvv (value) {
+    value.length = 3;
+    return true;
+}
+
+function finalStep () {
+    secondStep.classList.add('-hide');
+    finalPage.classList.remove('-hide');
+}
